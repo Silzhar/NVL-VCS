3194e7df235ffe77fa55daa4af860f1a76032abb
Silzhar <luis.ruizfer@gmail.com>
2019-12-16 17:39:43 +0100

    practica bucles

---
+++ NVL-HTML-P2a/ejercicios_js/NVL-JS-P10b.js
@@ -0,0 +1,21 @@
+
+for(var lista = []; lista.push(Math.floor((Math.random() * 100) + 1)); lista < 50);
+    var n = [];
+    var pares = [];
+    var impares = [];
+
+while (lista.length()){
+    if (lista %2 == 0){
+        pares.push(lista);
+    } else
+        impares.push(lista);
+
+}
+
+console.log(pares);
+console.log(impares);
+
+
+
+    
+
