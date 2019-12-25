82f22f206add0e38d8260357e722a35114bb3957
Silzhar <luis.ruizfer@gmail.com>
2019-12-25 13:02:42 +0100

    embeber php en html

---
+++ NVL-HTML-P2a/ejercicios_php/prueba_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prueba</title>
+</head>
+<body>
+    <p>Esto va a ser ignorado por PHP y mostrado por el navegador.</p>
+    <?php
+     echo 'Mientras que esto va a ser interpretado.'; 
+     ?>
+    <p>Esto también será ignorado por PHP y mostrado por el navegador.</p>
+</body>
+</html>
+
+
+
+
