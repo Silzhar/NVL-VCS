4b1551fa532cd6b8c3e8684525d10fdd1a3cf8b5
Silzhar <luis.ruizfer@gmail.com>
2019-12-29 16:51:21 +0100

    lectura ficheros php(Quijote)

---
+++ NVL-HTML-P2a/ejercicios_php/NVL-PHP-FICHEROS-1.php
@@ -0,0 +1,10 @@
+<?php
+$quijote = fopen('quijote.txt','r');
+
+while (($texto = fgets($quijote)) !== false) {
+    $molino = ['molino', 'Molino', 'MOLINO'];
+    $contador = 0;
+    if ($texto === $molino) {
+        $contador++;
+    };
+}
